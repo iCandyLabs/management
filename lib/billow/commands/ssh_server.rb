faca56bba1b07beed07ff1c51a17f247facd29fc
Steven Degutis <steven@cleancoders.com>
2014-04-05 15:56:03 -0500

    Fixing SSH.

--- lib/billow/commands/ssh_server.rb
+++ lib/billow/commands/ssh_server.rb
@@ -7,8 +7,8 @@ module Billow
     def call(server_name)
       server = get_server(server_name)
 
-      template = config.templates[server.template]
-      ssh_key_path = template[:ssh_key_path]
+      type = config.types[server.type]
+      ssh_key_path = type[:ssh_key_path]
       system("ssh -i #{ssh_key_path} root@#{server.public_ip_address}")
     end
 
