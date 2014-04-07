aa1b19c5ca647e3ea7ce3fcf48c5e2e6c65ded58
Steven Degutis <steven@cleancoders.com>
2014-04-07 17:58:46 -0500

    Show whether backups are active (always false for now).

--- lib/billow/commands/list_servers.rb
+++ lib/billow/commands/list_servers.rb
@@ -7,14 +7,14 @@ module Billow
     def call(env_name = nil)
       env = get_env(env_name)
 
-      format = "%-15s  %-10s  %-20s  %-20s\n"
+      format = "%-15s  %-10s  %-20s  %-20s  %-10s\n"
 
-      printf format, "Name", "State", "IP", "Private IP"
-      printf format, "-" * 15, "-" * 10, "-" * 20, "-" * 20
+      printf format, "Name", "State", "IP", "Private IP", "Backups?"
+      printf format, "-" * 15, "-" * 10, "-" * 20, "-" * 20, "-" * 10
 
       cloud.servers.each do |server|
         next if env_name && server.env != env_name
-        printf format, server.name, server.state, server.public_ip_address, server.private_ip_address
+        printf format, server.name, server.state, server.public_ip_address, server.private_ip_address, server.backups_active
       end
     end
 
