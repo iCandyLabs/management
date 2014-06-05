893241df4abb20f7a46f00cc56b839422dd9fde2
Steven Degutis <steven@cleancoders.com>
2014-06-05 13:15:33 -0500

    Simpler way to list server properties.

--- lib/billow/commands/list_servers.rb
+++ lib/billow/commands/list_servers.rb
@@ -8,14 +8,14 @@ module Billow
       env = get_env(env_name)
 
       cols = [
-        {size: 15, title: "Name",       fn: ->(s){ s.name }},
-        {size: 10, title: "State",      fn: ->(s){ s.state }},
-        {size: 20, title: "IP",         fn: ->(s){ s.public_ip_address }},
-        {size: 20, title: "Private IP", fn: ->(s){ s.private_ip_address }},
-        {size: 10, title: "Size",       fn: ->(s){ s.flavor_id }},
-        {size: 15, title: "Env",        fn: ->(s){ s.env }},
-        {size: 15, title: "Type",       fn: ->(s){ s.type }},
-        {size: 11, title: "EC2 ID",     fn: ->(s){ s.id }},
+        {size: 15, title: "Name",       fn: :name },
+        {size: 10, title: "State",      fn: :state },
+        {size: 20, title: "IP",         fn: :public_ip_address },
+        {size: 20, title: "Private IP", fn: :private_ip_address },
+        {size: 10, title: "Size",       fn: :flavor_id },
+        {size: 15, title: "Env",        fn: :env },
+        {size: 15, title: "Type",       fn: :type },
+        {size: 11, title: "EC2 ID",     fn: :id },
       ]
 
       format = cols.map{|c| "%-#{c[:size]}s"}.join("  ") + "\n"
@@ -28,7 +28,7 @@ module Billow
       cloud.servers.each do |server|
         next if env_name && server.env != env_name
 
-        row = [format].concat(cols.map{|c|c[:fn].call(server)})
+        row = [format].concat(cols.map{|c|server.send(c[:fn])})
         send :printf, *row
       end
     end
