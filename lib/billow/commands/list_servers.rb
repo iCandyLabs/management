0c9ad6cbdc5bbb0062abdf0b6d33bfebb0b57433
Steven Degutis <steven@cleancoders.com>
2014-06-05 17:03:50 -0500

    Renaming a variable.

--- lib/billow/commands/list_servers.rb
+++ lib/billow/commands/list_servers.rb
@@ -8,14 +8,14 @@ module Billow
       env = get_env(env_name)
 
       cols = [
-        {size: 20, title: "Name",       fn: :name },
-        {size: 10, title: "State",      fn: :state },
-        {size: 20, title: "IP",         fn: :public_ip_address },
-        {size: 20, title: "Private IP", fn: :private_ip_address },
-        {size: 10, title: "Size",       fn: :flavor_id },
-        {size: 15, title: "Env",        fn: :env },
-        {size: 15, title: "Type",       fn: :type },
-        {size: 11, title: "EC2 ID",     fn: :id },
+        {size: 20, title: "Name",       field: :name },
+        {size: 10, title: "State",      field: :state },
+        {size: 20, title: "IP",         field: :public_ip_address },
+        {size: 20, title: "Private IP", field: :private_ip_address },
+        {size: 10, title: "Size",       field: :flavor_id },
+        {size: 15, title: "Env",        field: :env },
+        {size: 15, title: "Type",       field: :type },
+        {size: 11, title: "EC2 ID",     field: :id },
       ]
 
       format = cols.map{|c| "%-#{c[:size]}s"}.join("  ") + "\n"
@@ -27,7 +27,7 @@ module Billow
         next if env_name && server.env != env_name
         next if server.state == 'terminated'
 
-        send :printf, *([format].concat(cols.map{|c|server.send(c[:fn])}))
+        send :printf, *([format].concat(cols.map{|c|server.send(c[:field])}))
       end
     end
 
