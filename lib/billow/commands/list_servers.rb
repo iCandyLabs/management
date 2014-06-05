cf15aed71f31c8bf2f8eac81cc7fdf1681d5eb31
Steven Degutis <steven@cleancoders.com>
2014-06-05 13:10:30 -0500

    More flexible way to list server properties.

--- lib/billow/commands/list_servers.rb
+++ lib/billow/commands/list_servers.rb
@@ -7,14 +7,29 @@ module Billow
     def call(env_name = nil)
       env = get_env(env_name)
 
-      format = "%-15s  %-10s  %-20s  %-20s  %-10s\n"
+      cols = [
+        {size: 15, title: "Name",       fn: ->(s){ s.name }},
+        {size: 10, title: "State",      fn: ->(s){ s.state }},
+        {size: 20, title: "IP",         fn: ->(s){ s.public_ip_address }},
+        {size: 20, title: "Private IP", fn: ->(s){ s.private_ip_address }},
+        {size: 10, title: "Size",       fn: ->(s){ s.flavor_id }},
+        {size: 15, title: "Env",        fn: ->(s){ s.env }},
+        {size: 15, title: "Type",       fn: ->(s){ s.type }},
+        {size: 11, title: "EC2 ID",     fn: ->(s){ s.id }},
+      ]
 
-      printf format, "Name", "State", "IP", "Private IP", "Backups?"
-      printf format, "-" * 15, "-" * 10, "-" * 20, "-" * 20, "-" * 10
+      format = cols.map{|c| "%-#{c[:size]}s"}.join("  ") + "\n"
+      header = [format].concat(cols.map{|c|c[:title]})
+      seps = [format].concat(cols.map{|c|'-' * c[:size]})
+
+      send :printf, *header
+      send :printf, *seps
 
       cloud.servers.each do |server|
         next if env_name && server.env != env_name
-        printf format, server.name, server.state, server.public_ip_address, server.private_ip_address, server.backups_active
+
+        row = [format].concat(cols.map{|c|c[:fn].call(server)})
+        send :printf, *row
       end
     end
 
