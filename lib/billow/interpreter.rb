3572d5c0214bc8d58bff859ea3a252dca8e9ddff
Steven Degutis <steven@cleancoders.com>
2014-06-07 18:20:06 -0500

    Better error handling on invalid arguments.

--- lib/billow/interpreter.rb
+++ lib/billow/interpreter.rb
@@ -18,15 +18,29 @@ module Billow
           opts.on('-v', '--version', 'Show version') { puts Billow::VERSION; exit }
         end
 
-        input << "-h" if input.empty?
+        abort parser.help if input.empty?
+
         args = parser.parse(input)
         task = args.shift
         ARGV.clear
 
         if chosen_command = commands.find{|c|c.command_name == task}
-          chosen_command.new.call(*args)
+          all_args = chosen_command.instance_method(:call).parameters
+          req_args = all_args.map(&:first).take_while{|p| p == :req}
+
+          case args.count
+          when args.count < req_args.count
+            puts "Error: not enough arguments"
+            abort parser.help
+          when args.count > all_args.count
+            puts "Error: too many arguments"
+            abort parser.help
+          else
+            chosen_command.new.call(*args)
+          end
         else
           puts "Error: unknown task \"#{task}\""
+          abort parser.help
         end
       end
 
